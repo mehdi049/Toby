2d09ae7269d04bdde2656b7fd6efa756d410ad85
Mehdi Marouani <Mehdi.Marouani@vistaprint.com>
2021-07-18 14:02:24 +0100

    - play video when displayed

--- assets/js/main.js
+++ assets/js/main.js
@@ -267,4 +267,17 @@
       mirror: false,
     });
   });
+
+  /**
+   * Play video when visible
+   */
+  window.addEventListener("scroll", () => {
+    $("video").each(function () {
+      if ($(this).is(":in-viewport")) {
+        $(this)[0].play();
+      } else {
+        $(this)[0].pause();
+      }
+    });
+  });
 })();
